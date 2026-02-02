6b090745be01a69b68087760bf7eed395ca29830
Bohdan Kasianchuk <bogdan.k1990@gmail.com>
2026-02-02 14:23:07 +0200

    Update global-error.tsx

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -6,7 +6,9 @@ export default function Error() {
             <body>
                 <main>
                     <div className="flex items-center justify-center w-full">
-                        <p className="text-5xl">Something globally went wrong</p>
+                        <p className="text-5xl font-bold leading-[1.2]">
+                            Something globally went wrong
+                        </p>
                     </div>
                 </main>
             </body>
