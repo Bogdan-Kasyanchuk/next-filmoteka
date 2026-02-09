67b48d2f3f4bdba1f928243875cdbe617ebb8c2f
Bohdan Kasianchuk <bogdan.k1990@gmail.com>
2026-02-09 18:09:07 +0200

    upd

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+    reactCompiler: true,
+    logging: {
+        fetches: {
+            fullUrl: true
+        }
+    },
+    experimental: {
+        inlineCss: true
+    },
     images: {
         remotePatterns: [
             {
